61eff1d03c4f5ee24b8418f0ccf48810dd97ac73
Shaker Ahamed <ahamedshaker7@gmail.com>
2022-04-29 16:54:51 +0600

    responsive navbar done

--- src/component/Shared/Navbar/Navbar.js
+++ src/component/Shared/Navbar/Navbar.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import logo from '../../../Images/Fruit Fest Logo.png';
 import CustomLink from '../../CustomLink/CustomLink';
-import { MenuAlt3Icon } from '@heroicons/react/solid'
+import { MenuAlt3Icon, XIcon } from '@heroicons/react/solid'
 import './Navbar.css'
 
 const Navbar = () => {
-    const [toggl, setToggl] = useState()
+    const [toggle, setToggle] = useState(false)
     return (
         <div className='nav-container'>
             <div className='nav-section'>
@@ -13,17 +13,24 @@ const Navbar = () => {
                     <img src={logo} alt="" />
                 </div>
                 <div>
-                    <nav className='nav-links'>
+                    <nav className={toggle ? 'nav-links-mobile' : 'nav-links'}>
                         <CustomLink to="/home">Home</CustomLink>
-                        <CustomLink to="/blog">Blog</CustomLink>
-                        <CustomLink to="/contact-us">Contact Us</CustomLink>
                         <CustomLink to="/manage-items">Manage Items</CustomLink>
                         <CustomLink to="/add-item">Add Item</CustomLink>
                         <CustomLink to="/my-item">My Items</CustomLink>
+                        <CustomLink to="/blog">Blog</CustomLink>
+                        <CustomLink to="/contact-us">Contact Us</CustomLink>
                         <CustomLink to="/log-out">Log Out</CustomLink>
                         <CustomLink to="/login">Login</CustomLink>
                     </nav>
-                    <div><MenuAlt3Icon className='icon'></MenuAlt3Icon></div>
+                    <div onClick={() => setToggle(!toggle)}>
+                        {
+                            toggle ? <XIcon className='icon'></XIcon>
+                                :
+                                <MenuAlt3Icon className='icon'></MenuAlt3Icon>
+                        }
+
+                    </div>
                 </div>
             </div>
         </div>
